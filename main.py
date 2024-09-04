63536eda7ff5253a6c19418db8313237f1cf663f
travon martin <martitra000@warren.k12.in.us>
2024-09-04 07:30:27 -0400

    48

---
+++ main.py
@@ -0,0 +1,7 @@
+name = input("What is your name:")
+
+droids = int(input("How many droids do you want?"))
+
+wookies = int(input("How many wookies do you want?"))
+
+print(name, "wants to meet", "droids and ", wookies, "Wookies!")
